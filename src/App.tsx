07433edfed50c07e686bde550fa40a5554232d73
Sylv Chen <sylv@udel.edu>
2026-08-27 17:58:35 -0400

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                Sylv Chen UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
+                edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
         </div>
